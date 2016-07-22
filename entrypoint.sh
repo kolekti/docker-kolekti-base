6c49686c9fa07dba66411add0e7ca26f76c78113
Samuel Maftoul <samuel.maftoul@gmail.com>
2016-07-22 15:57:14 +0200

    add copy of sqlite database in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,5 @@
 set -e
 
 cp /eLocus/src/kolekti_server/kolekti.ini /etc
+cp /eLocus/src/kolekti_server/db.sqlite3.ref /eLocus/src/kolekti_server/db.sqlite3
 LC_ALL=C PYTHONPATH=/eLocus/vendor:/eLocus/src:/eLocus/src/kolekti_server python eLocus/src/kolekti_server/manage.py runserver 0.0.0.0:8000
