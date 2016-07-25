bac020a1ddb05eae2acc720373e12a28e40bb732
Samuel Maftoul <samuel.maftoul@gmail.com>
2016-07-25 22:47:52 +0200

    fix entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,6 @@
 
 set -e
 
-export LC_ALL=C
-export PYTHONPATH=/kolekti/src:/kolekti/src/kolekti_server
-
-cp /kolekti/src/kolekti_server/kolekti.ini /etc
-
-python /kolekti/src/kolekti_server/manage.py syncdb --noinput
+PYTHONPATH=/kolekti/src:/kolekti/src/kolekti_server python /kolekti/src/kolekti_server/manage.py syncdb --noinput
 
 gunicorn -b 0.0.0.0:8000 --pythonpath /kolekti/src --chdir /kolekti/src/kolekti_server kolekti_server.wsgi
