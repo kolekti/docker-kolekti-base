1136f05f30b67ff1a4ee8000328e552326069ed6
Samuel Maftoul <samuel.maftoul@gmail.com>
2016-08-02 15:30:35 +0200

    allow to pass options to gunicorn

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ set -e
 
 PYTHONPATH=/kolekti/src:/kolekti/src/kolekti_server python /kolekti/src/kolekti_server/manage.py syncdb --noinput
 
-gunicorn -b 0.0.0.0:8000 --pythonpath /kolekti/src --chdir /kolekti/src/kolekti_server kolekti_server.wsgi
+gunicorn -b 0.0.0.0:8000 --pythonpath /kolekti/src --chdir /kolekti/src/kolekti_server ${GUNICORN_OPTS} kolekti_server.wsgi
