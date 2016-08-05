b171be6864153ff141b3aadf922afe0fcdc17248
Samuel Maftoul <samuel.maftoul@gmail.com>
2016-08-05 22:06:42 +0200

    manage --user option of gunicorn

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,8 @@ set -e
 
 PYTHONPATH=/kolekti/src:/kolekti/src/kolekti_server python /kolekti/src/kolekti_server/manage.py syncdb --noinput
 
-gunicorn -b 0.0.0.0:8000 --pythonpath /kolekti/src --chdir /kolekti/src/kolekti_server ${GUNICORN_OPTS} kolekti_server.wsgi
+if [ -z "$UID" ] ; then
+  UID=0
+fi
+
+gunicorn -b 0.0.0.0:8000 --pythonpath /kolekti/src --chdir /kolekti/src/kolekti_server --user=${UID} ${GUNICORN_OPTS} kolekti_server.wsgi
