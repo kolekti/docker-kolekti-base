7ad96df3313dfa6bff24b8e199d4f524ca070e36
Stéphane Bonhomme <stephane@exselt.com>
2017-05-04 22:58:20 +0200

    run manage.py migrate as 1001 instead of syncdb as root

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM debian:jessie
 
 RUN apt-get update && apt-get install -y \
+      sudo \
       gunicorn                   \
       python-lxml                \
       python-pil                 \
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,10 @@
 
 set -e
 
-PYTHONPATH=/kolekti/src:/kolekti/src/kolekti_server python /kolekti/src/kolekti_server/manage.py syncdb --noinput
-
 if [ -z "$UID" ] ; then
   UID=0
 fi
+
+sudo -E -H -u \#${UID} PYTHONPATH=/kolekti/src:/kolekti/src/kolekti_server python /kolekti/src/kolekti_server/manage.py migrate --noinput
 
 gunicorn -b 0.0.0.0:8000 --pythonpath /kolekti/src --chdir /kolekti/src/kolekti_server --user=${UID} ${GUNICORN_OPTS} kolekti_server.wsgi
