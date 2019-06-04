7e2c4aa36d79beba20968c1675ec156fe2d31e85
Stéphane Bonhomme <stephane@exselt.com>
2019-06-04 16:48:26 +0200

    adds update-hooks in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,5 +17,6 @@ export LD_LIBRARY_PATH=/usr/local/lib
 cd /kolekti/src/kolekti_server
 sudo -E -H -u \#${UID} PYTHONPATH=/kolekti/src:/kolekti/src/kolekti_server python /kolekti/src/kolekti_server/manage.py migrate --noinput
 sudo -E -H -u \#${UID} PYTHONPATH=/kolekti/src:/kolekti/src/kolekti_server python /kolekti/src/kolekti_server/manage.py collectstatic --noinput
+sudo -E -H -u \#${UID} PYTHONPATH=/kolekti/src:/kolekti/src/kolekti_server python /kolekti/src/kolekti_server/manage.py update_hooks --noinput
 
 gunicorn -b 0.0.0.0:8000 --pythonpath /kolekti/src --chdir /kolekti/src/kolekti_server --user=${UID} ${GUNICORN_OPTS} kolekti_server.wsgi
